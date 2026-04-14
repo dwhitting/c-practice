360ae33141cf4845b91687f7fb716f50a9b98e5a
Drew Whitting <dwhitting@me.com>
2026-04-13 20:38:06 -0400

    updates

---
+++ king_chap_15/test_calc.c
@@ -0,0 +1,8 @@
+#include "test_external_file.h"
+
+int main(void) {
+
+    print_func();
+
+    return 0;
+}
---
+++ king_chap_15/test_external_file.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test_external_file.h"
+
+void print_func(void) {
+    printf("The function printed\n");
+}
