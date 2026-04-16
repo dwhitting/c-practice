551c698b93173345076d92020e83f82d2d00918f
Drew Whitting <dwhitting@me.com>
2026-04-15 21:41:34 -0400

    updates

--- king_chap_17/remind2.c
+++ king_chap_17/remind2.c
@@ -21,8 +21,43 @@ int main(void)
 
         printf("Enter day and reminder: ");
         scanf("%2d", &day);
+        if (day == 0)
+            break;
+        sprintf(day_str, "%2d", day);
+        read_line(msg_str, MSG_LEN);
+
+        for (i = 0; i < num_remind; i++)
+            if (strcmp(day_str, reminders[i]) < 0)
+                break;
+        for (j = num_remind; j > i; j--)
+            reminders[j] = reminders[j-1];
+
+        reminders[i] = malloc(2 + strlen(msg_str) + 1);
+        if (reminders[i] == NULL) {
+            printf("-- No Space Left --\n");
+            break;
+        }
+
+        strcpy(reminders[i], day_str);
+        strcat(reminders[i], msg_str);
+
+        num_remind++;
     }
 
+    printf("\nDay Reminder\n");
+    for (i = 0; i < num_remind; i++)
+        printf(" %s\n", reminders[i]);
 
     return 0;
+}
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')            
+        if (i < n)
+            str[i++] = ch;
+    str[i] = '\0';
+    return i;    
 }
