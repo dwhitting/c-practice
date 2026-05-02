e30f596d1041ee6b7ed1e7009c9e7e2f347136e3
Drew Whitting <dwhitting@me.com>
2026-05-02 10:52:44 -0400

    UDs

--- process_text_files/main.c
+++ process_text_files/main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
-int main(void)
+int main(int args, char *argv[])
 {
-    printf("test\n");
+    if (args < 2) {
+        fprintf(stderr, "usage: wfreq file1 [file2]\n");
+        return 1;
+    }
+
+    FILE *fptr;
+    char buff[255];
+
+    int ch;
+    int bytecnt;
+    int linecnt;
+
+    for (int i = 1; i < args; i++) {
+        bytecnt = 0;
+        linecnt = 1;
+        fptr = fopen(argv[i], "r");
+
+        if (fptr == NULL) {
+            printf("Error: could not open file\n");
+            //fclose(fptr);
+            return 1;
+        }
+
+        while ((ch = fgetc(fptr)) != EOF) {
+            bytecnt++;
+            if (ch == '\n') linecnt++;
+        }
+        printf("For %s\n", argv[i]);
+        printf("bytes: %d\n", bytecnt);
+        printf("lines: %d\n", linecnt);
+        printf("\n");
+        fclose(fptr);
+    }
+
+
+
+    // while (fgets(buff, 255, fptr)) {
+    //     printf("%s", buff);
+    // }
+
     return 0;
 }
