83a59b4bdaef7b720ce1c37081e0740409727f16
Drew Whitting <dwhitting@me.com>
2026-05-15 22:29:47 -0400

    UDs

---
+++ OSs_Three_Easy_Pieces/threads/main.c
@@ -0,0 +1,8 @@
+#include "thread_0.h"
+
+int main(void)
+{
+    run_thread_0();
+
+    return 0;
+}
---
+++ OSs_Three_Easy_Pieces/threads/thread_0.h
@@ -0,0 +1,8 @@
+#ifndef THREAD_0_H
+#define THREAD_0_H
+#include "thread_0.c"
+
+void *mythread(void *);
+int run_thread_0(void);
+
+#endif
