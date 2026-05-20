93cc2dcea7715c72df9cf59c3632f4aa0b03e6db
dwhitting <dwhitting@me.com>
2026-05-19 21:30:36 -0400

    UDs

--- pointer_prac/main.c
+++ pointer_prac/main.c
@@ -3,43 +3,9 @@
 
 int main(void)
 {
-    int **arr;
+    if (0) return 1;
 
-    arr = (int **) malloc(sizeof(int *) * 5);
-    if (arr == NULL) return 1;
-
-    arr[0] = (int *) malloc(sizeof(int) * 5);
-    if (arr[0] == NULL) return 1;
-
-    arr[4] = (int *) malloc(sizeof(int) * 20);
-    if (arr[4] == NULL) return 1;
-
-    arr[0][0] = 7;
-    arr[4][19] = 33;
-
-    printf("value[0][0]: %d\n", arr[0][0]);
-    printf("value [4][19]: %d\n", arr[4][19]);
-
-    free(arr[0]);
-    free(arr[4]);
-    free(arr);
-
-    return 0;
-}
-
-int from_c_programming(void) {
-    char *fruit[] = {
-        "watermelon",
-        "banana",
-        "pear",
-        "apple"
-    };
-
-    for (int x = 0; x < 4; x++) {
-        putchar(**(fruit+x));
-        putchar('\n');
-    }
-
+    printf("after error\n");
 
     return 0;
 }
