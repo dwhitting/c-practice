ff9a3f20bbead42e15f70ca96ed254301218af30
Drew Whitting <dwhitting@me.com>
2026-06-07 19:55:46 -0400

    UDs

---
+++ cs-app/11_chap/tiny.c
@@ -0,0 +1,50 @@
+/*
+ * tiny.c - a simple, iterative HTTP/1.0 web server that uses the 
+ * GET method to serve static and dynamic content */
+#include "rio.h"
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int df, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum,
+                char *shortmsg, char *longmsg);
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* check command-line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    listenfd = open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
+        getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE,
+                    port, MAXLINE, 0);
+        printf("accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);
+        close(connfd);  
+    } 
+        
+    //return 0; 
+}
+
+void doit(int fd) {
+    int is_static;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;
+
+    /* reead request line and headers */
+}
