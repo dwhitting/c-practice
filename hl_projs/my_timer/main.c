110268ced048ad254ba312cb0a1d037ad0bfe996
Drew Whitting <dwhitting@me.com>
2026-06-09 14:07:00 -0400

    UDs

--- hl_projs/my_timer/main.c
+++ hl_projs/my_timer/main.c
@@ -1,60 +1,28 @@
 #include "my_timer.h"
-
-int arr_comp();
+#include <sys/types.h>
+#include <string.h>
 
 int main(void)
 {
-    srand((unsigned int)time(NULL));
+    char template[] = "/tmp/testXXXXXX";
+    char test[20];
 
-    my_timer_t timer;
+    setbuf(stdout, NULL);
+    
+    int fd = mkstemp(template);
+    printf("file name: %s\n", template);
 
-    begin_timer(&timer);
+    pid_t proc_pid;
 
-    //sleep(2);
-    arr_comp();
-
-    end_timer(&timer);    
-
-    return 0;
-}
-
-int arr_comp(void) {
-    int rows = 10000;
-    int cols = 10000;
-
-    int **arr;
-    arr = (int **) malloc(rows * sizeof(int *));
-    if (arr == NULL) {
-        printf("memory allocation failed\n");
-        return 1;
+    switch (proc_pid = fork()) {
+        
+        case 0:
+            strcpy(test, "child first\n");
+            write(fd, test, 20);
+            exit(0);
+        default:  /* parent */
+            strcpy(test, "parent second\n");
+            write(fd, test, 20);
     }
-
-    for (int i = 0; i < rows; i++) {
-        arr[i] = (int *) malloc(cols * sizeof(int));
-        if (arr[i] == NULL) {
-            printf("memory allocation failed\n");
-            return 1;            
-        }
-    }
-
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            arr[row][col] = (rand() % 10) +1;
-        }
-    }
-
-    long sum = 0;
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            sum += arr[row][col];
-        }
-    }
-    printf("sum: %ld\n", sum);
-
-    for (int row = 0; row < rows; row++) {
-        free(arr[row]);       
-    }
-    free(arr);
-
-    return 0;
+    return 0;    
 }
