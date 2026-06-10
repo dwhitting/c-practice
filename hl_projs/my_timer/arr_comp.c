6ab3fac81f81a34925cac7c4f6d7e2304f463781
Drew Whitting <dwhitting@me.com>
2026-06-10 06:30:24 -0400

    UDs

---
+++ hl_projs/my_timer/arr_comp.c
@@ -0,0 +1,60 @@
+#include "my_timer.h"
+
+int arr_comp();
+
+int main(void)
+{
+    srand((unsigned int)time(NULL));
+
+    my_timer_t timer;
+
+    begin_timer(&timer);
+
+    //sleep(2);
+    arr_comp();
+
+    end_timer(&timer);    
+
+    return 0;
+}
+
+int arr_comp(void) {
+    int rows = 10000;
+    int cols = 10000;
+
+    int **arr;
+    arr = (int **) malloc(rows * sizeof(int *));
+    if (arr == NULL) {
+        printf("memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        arr[i] = (int *) malloc(cols * sizeof(int));
+        if (arr[i] == NULL) {
+            printf("memory allocation failed\n");
+            return 1;            
+        }
+    }
+
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            arr[row][col] = (rand() % 10) +1;
+        }
+    }
+
+    long sum = 0;
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            sum += arr[row][col];
+        }
+    }
+    printf("sum: %ld\n", sum);
+
+    for (int row = 0; row < rows; row++) {
+        free(arr[row]);       
+    }
+    free(arr);
+
+    return 0;
+}
--- hl_projs/my_timer/main.c
+++ hl_projs/my_timer/main.c
@@ -1,28 +1,47 @@
 #include "my_timer.h"
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 
 int main(void)
 {
-    char template[] = "/tmp/testXXXXXX";
-    char test[20];
+    char template[] = "/tmp/tempFileXXXXXX";
+    char test[50];
+    char read_string[100] = "\0";
 
     setbuf(stdout, NULL);
     
     int fd = mkstemp(template);
     printf("file name: %s\n", template);
 
-    pid_t proc_pid;
+    pid_t child_pid, parent_pid;
 
-    switch (proc_pid = fork()) {
+    switch (child_pid = fork()) {
         
         case 0:
             strcpy(test, "child first\n");
             write(fd, test, 20);
             exit(0);
         default:  /* parent */
+            parent_pid = getpid();
             strcpy(test, "parent second\n");
             write(fd, test, 20);
+            //wait(NULL);
+            
     }
+    printf("child pid: %d\n", child_pid);
+    printf("parent pid: %d\n", parent_pid);
+    lseek(fd, 0, SEEK_SET);
+  
+    strcpy(test, "");
+    ssize_t bytes_read;
+    
+    while ((bytes_read = read(fd, test, sizeof(test) - 1)) != 0) {
+        test[bytes_read] = '\0';
+        strcat(read_string, test);
+    }
+    
+    printf("bytes: %ld\n", strlen(read_string));
+    printf("string: %s\n", read_string);
     return 0;    
 }
