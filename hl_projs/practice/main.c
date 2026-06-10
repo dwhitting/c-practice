f6911b82ecc1576ef6225ee40f9a872b156e7b2b
Drew Whitting <dwhitting@me.com>
2026-06-10 17:05:11 -0400

    UDs

--- hl_projs/practice/main.c
+++ hl_projs/practice/main.c
@@ -2,23 +2,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <errno.h>
 
 int main(void)
 {
     int rows = 10000;
     int cols = 10000;
-
-    char template[] = "/tmp/tempFileXXXXXX";
-    long cp_input = 0;
-    long read_string = 0;
+    int my_pipe[2];
+    long received_val = 0;
     long proc_sum = 0;
+
     my_timer_t timer;
 
-    setbuf(stdout, NULL);
-    
-    int fd = mkstemp(template);
-    printf("file name: %s\n", template);
+    /* create pipe
+     * [1] writes data, [0] receives data */
+    if (pipe(my_pipe) == -1) {
+        printf("Pipe create failed\n");
+        exit(1);
+    }
 
+    /* make the array */
     int **arr;
     arr = (int **) malloc(rows * sizeof(int *));
     if (arr == NULL) {
@@ -34,6 +37,7 @@ int main(void)
         }
     }
 
+    /* fill the array with 'random' numbers */
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
             arr[row][col] = (rand() % 10) +1;
@@ -46,40 +50,38 @@ int main(void)
     switch (child_pid = fork()) {
         
         case 0:
+            close(my_pipe[0]); /* because parent will be receiving */
             for (int row = 0; row < (rows / 2); row++) {
                 for (int col = 0; col < cols; col++) {
                     proc_sum += arr[row][col];
                 }
             }
-            write(fd, &proc_sum, sizeof(cp_input));
+            write(my_pipe[1], &proc_sum, sizeof(proc_sum));
+            close(my_pipe[1]);
             exit(0);
         default:  /* parent */
+            close(my_pipe[1]);
             parent_pid = getpid();
             for (int row = (rows / 2); row < rows; row++) {
                 for (int col = 0; col < cols; col++) {
                     proc_sum += arr[row][col];
                 }
             }
-            write(fd, &proc_sum, sizeof(cp_input));
-            wait(NULL);
-            
+            read(my_pipe[0], &received_val, sizeof(received_val));
+            close(my_pipe[0]);
+            proc_sum += received_val; /* final sum of both chunks of the array */
+            wait(NULL);            
     }
     end_timer(&timer);
 
     printf("child pid: %d\n", child_pid);
     printf("parent pid: %d\n", parent_pid);
-    lseek(fd, 0, SEEK_SET);
-  
-    ssize_t bytes_read;
-    char read_bytes[50];
-    int posit = 0;
-    long res_sum = 0;
-    
-    while ((bytes_read = read(fd, read_bytes, 8)) != 0) {
-        res_sum += *(long *)read_bytes;
-    }
 
-    printf("Final sum: %ld\n", res_sum);
+    printf("Final sum: %ld\n", proc_sum);
+
+    for (int row = 0; row < rows; row++)
+        free(arr[row]);
+    free(arr);
     
     return 0;    
 }
