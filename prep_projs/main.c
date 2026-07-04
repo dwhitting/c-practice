f335a39b61fe83f5aa9b25be4a7e06f47b022ef1
Drew Whitting <dwhitting@me.com>
2026-07-03 22:29:14 -0400

    uds

--- prep_projs/main.c
+++ prep_projs/main.c
@@ -5,22 +5,33 @@
 #include <signal.h>
 
 static void sigHandler(int sig) {
-    printf("Signal hit\n");
+    static int count = 0;
+
+    /* Unsafe. Uses non-async sig functions */
+    if (sig == SIGINT) {
+        count++;
+        printf("Caught SIGINT (%d)\n", count);
+        return;     /* resumes at point of interupt */
+    }
+
+    /* must be SIGQUIT if gets here */
+    printf("Caught SIGQUIT\n");
+    exit(EXIT_SUCCESS);
 }
 
 int main() {
-    int j;
 
     if (signal(SIGINT, sigHandler) == SIG_ERR) {
         perror("signal");
         exit(1);
     }
 
-    for (j = 0; ; j++) {
-        printf("%d\n", j);
-        sleep(3);
+    if (signal(SIGQUIT, sigHandler) == SIG_ERR) {
+        perror("signal");
     }
-    
 
+    for (;;)        
+        pause();    /* blocks until signal is caught */
+    
     return 0;
 }
---
+++ prep_projs/prep_saves-reuse/first_sig_hdlr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+
+static void sigHandler(int sig) {
+    printf("Sig: %d\n", sig);
+    printf("Signal hit\n");
+}
+
+int main() {
+    int j;
+
+    if (signal(SIGINT, sigHandler) == SIG_ERR) {
+        
+        perror("signal");
+        exit(1);
+    }
+
+    for (j = 0; ; j++) {
+        printf("%d\n", j);
+        sleep(3);
+    }
+    
+
+    return 0;
+}
